fc1a478ea6569ebfccfc970f9d46d4d0812390e7
Marcus Batista <marcus_batista@eticalgarve.com>
2024-01-17 19:51:34 +0000

    first commit

---
+++ aula_2/app.js
@@ -0,0 +1,17 @@
+const resultmultiple = (a,b,c,d )=> {
+    
+    return [a,b, c,d];
+
+}
+
+let a = resultmultiple(1,2,3)[0];
+let b = resultmultiple(1,2,3)[1];
+let d = resultmultiple(1,2,3)[2];   
+let c = resultmultiple(1,2,3)[3];
+
+
+console.log(a);
+console.log(b);
+console.log("ataaaaaao");
+console.log(("comkie" + "  " +"puto"));
+
