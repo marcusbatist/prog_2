5c8ef84136728a4d1f7f59ce87b684b43671c878
Marcus Batista <marcus_batista@eticalgarve.com>
2024-01-17 21:11:34 +0000

    update durante a aula

---
+++ aula_3/app.js
@@ -0,0 +1,52 @@
+/* 
+let a = 10;
+let b = "100";
+
+if(a === "10" && b == "100") {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+if(a === "10" || b == "100") {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+else if(a === "30"){
+    console.log(false);
+}else {
+    console.log("erro");
+}
+
+
+switch(a){
+    case 10:
+        console.log(true);
+    break;
+    case "10":
+        console.log(false);
+    break;
+    case "20":
+        console.log(false);
+    break;
+    case "40":
+        console.log(false);
+    break;
+    default:
+        console.log(true);
+}
+*/
+
+let a = 0;
+
+
+while (a < 10 ) {
+    console.log("a", a);
+    
+
+a = a +1;
+
+
+}
