e80fd27881c87fe571be86eb95d92e4cdb7790b3
Marcus Batista <marcus_batista@eticalgarve.com>
2024-01-25 21:47:28 +0000

    aula_9 exercicios

---
+++ aula_9/app.js
@@ -0,0 +1,84 @@
+console.log("1");
+
+
+window.onload = () =>{
+    console.log("documento carregado, começar aplicação");
+}
+
+console.log("2");
+
+
+
+
+
+    let a = 100;
+    let b = 233;
+
+const funcao = (a,b) => {
+    
+    return a + b ;
+
+}
+const result = funcao(a,b);
+console.log(result);
+
+let multi = result * 15685456 ;
+
+console.log(multi)
+
+
+
+
+
+let c = 100;
+let d = 233;
+
+const conta2 = (c,d) => {
+
+return c * d ;
+
+}
+const resul = conta2(c,d);
+console.log(resul);
+
+/*
+
+let SalarioMin = 820;
+let Irs = 0.11
+const calcular = () => {
+
+}
+*/
+
+const array = [2,45,34,2];
+let soma = 0;
+
+for (let i = 0; i < array.length; i++) {
+    soma += array[i];
+  }
+  console.log("A soma dos elementos é:", soma);
+
+  /*
+const calAverage = (num)=> {
+
+    let value = 0;
+    num.forEach( num => {
+        value += num;
+    });
+
+    return value / num.length;
+
+}
+
+const result4 = 
+*/
+
+const array2 = ["SE ESTAS A LER ISTO E POR QUE ÉS ...", "Gay"]
+
+
+    array2.forEach( num => {
+        console.log(num);
+    });
+
+
+
