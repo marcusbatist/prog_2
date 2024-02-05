a07fc1d53e88da20c5bee2ccd6480d6fdacf1b5e
Marcus Batista <marcus_batista@eticalgarve.com>
2024-02-05 21:19:56 +0000

    Projeto Final

---
+++ Projecto/app.js
@@ -0,0 +1,36 @@
+/*
+//variaveis - let;
+
+let
+
+
+//variaveis - const;
+
+const 
+*/
+
+//window.onload;
+
+window.onload = () => {
+
+   
+
+}
+
+
+
+// data.json
+
+/*
+
+[
+    {
+        "type":,
+        "title":,
+        "medium":,
+        "image":,
+        "artist":,
+    }
+]
+
+*/
